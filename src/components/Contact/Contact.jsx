272155f341f98c6f1b4bf9219073d90d1bb321a0
Aniket Jain <aniket7247213350@gmail,com>
2025-07-21 21:28:23 +0530

    initial

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:aniket7247213350@gmail.com">aniket7247213350@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/aniket-jain-1a5450257?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">linkedin.com/Aniket</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/Aniketjain15">github.com/Aniket</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
---
+++ src/components/Projects/ProjectCard.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import styles from "./ProjectCard.module.css";
+import { getImageUrl } from "../../utils";
+
+export const ProjectCard = ({
+  project: { title, imageSrc, description, skills, LiveLink },
+}) => {
+  return (
+    <div className={styles.container}>
+      <img
+        src={getImageUrl(imageSrc)}
+        alt={`Image of ${title}`}
+        className={styles.image}
+      />
+      <h3 className={styles.title}>{title}</h3>
+      <p className={styles.description}>{description}</p>
+      <ul className={styles.skills}>
+        {skills.map((skill, id) => {
+          return (
+            <li key={id} className={styles.skill}>
+              {skill}
+            </li>
+          );
+        })}
+      </ul>
+      <div className={styles.links}>
+       
+        <a href={LiveLink} className={styles.link}>
+          LiveLink
+        </a>
+      </div>
+    </div>
+  );
+};
